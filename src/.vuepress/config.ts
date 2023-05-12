d237079a96ddb327162799259856f9e343dd8f45
yinyu985 <yinyu985@gmail.com>
2023-05-12 23:16:28 +0800

    test4

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -3,7 +3,7 @@ import theme from "./theme.js";
 import { searchProPlugin } from "vuepress-plugin-search-pro";
 
 export default defineUserConfig({
-  base: "/docs/",
+  base: "/",
   lang: "zh-CN",
   theme,
   plugins: [
