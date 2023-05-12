256e9118ac35111ae0691037bb9804e4932c3e61
yinyu985 <yinyu985@gmail.com>
2023-05-12 23:58:56 +0800

    init

--- src/.vuepress/navbar/en.ts
+++ src/.vuepress/navbar/en.ts
@@ -1,11 +1,8 @@
 import { navbar } from "vuepress-theme-hope";
 
 export const enNavbar = navbar([
-  // "/docs/",
-  { text: "主页", icon: "discover", link: "/home" },
-  { text: "", icon: "discover", link: "/home" },
+  { text: "主页", icon: "discover", link: "/home/" },
   { text: "文档", icon: "discover", link: "/docs/"},
-  { text: "", icon: "discover", link: "/docs/"},
   { text: "随笔", icon: "discover", link: "/blog/"},
   // { text: "ELK", icon: "discover", link: "/docs/ELK/" },
   // { text: "Other", icon: "discover", link: "/docs/Other" },
